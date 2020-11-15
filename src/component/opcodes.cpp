efa520c8da9bcfb3e0e6811fd9aa8fa636bdc0bd
chortlesource <chris.m.short@outlook.com>
2020-11-15 16:32:59 +0000

    Implemented opcodes

--- src/component/opcodes.cpp
+++ src/component/opcodes.cpp
@@ -76,7 +76,7 @@ void CPU::opcode_and(uint16_t const& value) {
 }
 
 
-// ASL - AArithmetic Shift Left (Memory)
+// ASL - Arithmetic Shift Left (Memory)
 void CPU::opcode_asl(uint16_t const& value) {
   uint8_t m = memory[value];
   memory[value] = m << 1;
@@ -87,7 +87,7 @@ void CPU::opcode_asl(uint16_t const& value) {
 }
 
 
-// ASL - AArithmetic Shift Left (Accumulator)
+// ASL - Arithmetic Shift Left (Accumulator)
 void CPU::opcode_asl_acc(uint16_t const& value) {
   uint8_t a = state.a;
   state.a = a << 1;
@@ -206,55 +206,375 @@ void CPU::opcode_cmp(uint16_t const& value) {
 }
 
 
-void CPU::opcode_cpx(uint16_t const& value) {}
-void CPU::opcode_cpy(uint16_t const& value) {}
+// CPX - Compare X Register
+void CPU::opcode_cpx(uint16_t const& value) {
+  uint8_t v = state.x - memory[value];
+  // Check flags
+  set_zero(!(v & 0xFF), state.flags);
+  set_negative(v & 0x80, state.flags);
+  set_carry((state.x >= memory[value]), state.flags);
+}
 
-void CPU::opcode_dec(uint16_t const& value) {}
-void CPU::opcode_dex(uint16_t const& value) {}
-void CPU::opcode_dey(uint16_t const& value) {}
 
-void CPU::opcode_eor(uint16_t const& value) {}
+// CPY - Compare Y Register
+void CPU::opcode_cpy(uint16_t const& value) {
+  uint8_t v = state.y - memory[value];
+  // Check flags
+  set_zero(!(v & 0xFF), state.flags);
+  set_negative(v & 0x80, state.flags);
+  set_carry((state.y >= memory[value]), state.flags);
+}
 
-void CPU::opcode_inc(uint16_t const& value) {}
-void CPU::opcode_inx(uint16_t const& value) {}
-void CPU::opcode_iny(uint16_t const& value) {}
 
-void CPU::opcode_jmp(uint16_t const& value) {}
-void CPU::opcode_jsr(uint16_t const& value) {}
+// DEC - Decrement Memory
+void CPU::opcode_dec(uint16_t const& value) {
+  uint8_t m     = memory[value] - 1;
+  memory[value] = m;
+  // Check flags
+  set_zero(!(m & 0xFF), state.flags);
+  set_negative(m & 0x80, state.flags);
+}
 
-void CPU::opcode_lda(uint16_t const& value) {}
-void CPU::opcode_ldx(uint16_t const& value) {}
-void CPU::opcode_ldy(uint16_t const& value) {}
-void CPU::opcode_lsr(uint16_t const& value) {}
-void CPU::opcode_lsr_acc(uint16_t const& value) {}
 
-void CPU::opcode_nop(uint16_t const& value) {}
+// DEX - Decrement X Register
+void CPU::opcode_dex(uint16_t const& value) {
+  uint8_t x = state.x - 1;
+  state.x   = x;
+  // Check flags
+  set_zero(!(x & 0xFF), state.flags);
+  set_negative(x & 0x80, state.flags);
+}
 
-void CPU::opcode_ora(uint16_t const& value) {}
 
-void CPU::opcode_pha(uint16_t const& value) {}
-void CPU::opcode_php(uint16_t const& value) {}
-void CPU::opcode_pla(uint16_t const& value) {}
-void CPU::opcode_plp(uint16_t const& value) {}
+// DEY - Decrement Y Register
+void CPU::opcode_dey(uint16_t const& value) {
+  uint8_t y = state.y - 1;
+  state.y   = y;
+  // Check flags
+  set_zero(!(y & 0xFF), state.flags);
+  set_negative(y & 0x80, state.flags);
+}
 
-void CPU::opcode_rol(uint16_t const& value) {}
-void CPU::opcode_rol_acc(uint16_t const& value) {}
-void CPU::opcode_ror(uint16_t const& value) {}
-void CPU::opcode_ror_acc(uint16_t const& value) {}
-void CPU::opcode_rti(uint16_t const& value) {}
-void CPU::opcode_rts(uint16_t const& value) {}
 
-void CPU::opcode_sbc(uint16_t const& value) {}
-void CPU::opcode_sec(uint16_t const& value) {}
-void CPU::opcode_sed(uint16_t const& value) {}
-void CPU::opcode_sei(uint16_t const& value) {}
-void CPU::opcode_sta(uint16_t const& value) {}
-void CPU::opcode_stx(uint16_t const& value) {}
-void CPU::opcode_sty(uint16_t const& value) {}
 
-void CPU::opcode_tax(uint16_t const& value) {}
-void CPU::opcode_tay(uint16_t const& value) {}
-void CPU::opcode_tsx(uint16_t const& value) {}
-void CPU::opcode_txa(uint16_t const& value) {}
-void CPU::opcode_txs(uint16_t const& value) {}
-void CPU::opcode_tya(uint16_t const& value) {}
+// EOR - Exclusive OR
+void CPU::opcode_eor(uint16_t const& value) {
+  state.a ^= memory[value];
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_negative(state.a & 0x80, state.flags);
+}
+
+
+// INC - Increment Memory
+void CPU::opcode_inc(uint16_t const& value) {
+  uint8_t m     = memory[value] + 1;
+  memory[value] = m;
+  // Check flags
+  set_zero(!(m & 0xFF), state.flags);
+  set_negative(m & 0x80, state.flags);
+}
+
+
+// INX - Increment X Register
+void CPU::opcode_inx(uint16_t const& value) {
+  uint8_t x = state.x + 1;
+  state.x   = x;
+  // Check flags
+  set_zero(!(x & 0xFF), state.flags);
+  set_negative(x & 0x80, state.flags);
+}
+
+
+// INY - Increment Y Register
+void CPU::opcode_iny(uint16_t const& value) {
+  uint8_t y = state.y + 1;
+  state.y   = y;
+  // Check flags
+  set_zero(!(y & 0xFF), state.flags);
+  set_negative(y & 0x80, state.flags);
+}
+
+
+// JMP - Jump
+void CPU::opcode_jmp(uint16_t const& value) {
+  state.pc = value;
+}
+
+
+// JSR - Jump to Subroutine
+void CPU::opcode_jsr(uint16_t const& value) {
+  uint16_t addr = state.pc - 1;
+  stack_push((addr >> 8) & 0xFF);  // Push high byte
+  stack_push(addr & 0xFF);         // Push low byte
+  state.pc = value;
+}
+
+
+// LDA - Load Accumulator
+void CPU::opcode_lda(uint16_t const& value) {
+  state.a = memory[value];
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_negative(state.a & 0x80, state.flags);
+}
+
+
+// LDX - Load X Register
+void CPU::opcode_ldx(uint16_t const& value) {
+  state.x = memory[value];
+  // Check flags
+  set_zero(!(state.x & 0xFF), state.flags);
+  set_negative(state.x & 0x80, state.flags);
+}
+
+
+// LDY - Load Y Register
+void CPU::opcode_ldy(uint16_t const& value) {
+  state.y = memory[value];
+  // Check flags
+  set_zero(!(state.y & 0xFF), state.flags);
+  set_negative(state.y & 0x80, state.flags);
+}
+
+
+// LSR - Logical Shift Right (Memory)
+void CPU::opcode_lsr(uint16_t const& value) {
+  uint8_t m = memory[value];
+  memory[value] = m >> 1;
+  // Check flags
+  set_zero(!(m & 0xFF), state.flags);
+  set_carry(m & 0x01, state.flags);
+  set_negative(m & 0x80, state.flags);
+}
+
+
+// LSR - Logical Shift Right (Accumulator)
+void CPU::opcode_lsr_acc(uint16_t const& value) {
+  uint8_t a = state.a;
+  state.a = a >> 1;
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags); // If zero set flag
+  set_carry(a & 0x01, state.flags);         // If 7 bit is set, set carry
+  set_negative(a & 0x80, state.flags);      // If bit 7 is set number is negative
+}
+
+
+// NOP - No Operation
+void CPU::opcode_nop(uint16_t const& value) {
+  // No operation
+}
+
+
+// ORA - Logical Inclusive OR
+void CPU::opcode_ora(uint16_t const& value) {
+  state.a |= memory[value];
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_negative(state.a & 0x80, state.flags);
+}
+
+
+// PHA - Push Accumulator
+void CPU::opcode_pha(uint16_t const& value) {
+  stack_push(state.a);
+}
+
+
+// PHP - Push Processor Status
+void CPU::opcode_php(uint16_t const& value) {
+  stack_push(state.flags);
+}
+
+
+// PLA - Pull Accumulator
+void CPU::opcode_pla(uint16_t const& value) {
+  state.a = stack_pop();
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_negative(state.a & 0x80, state.flags);
+}
+
+
+// PLP - Pull Processor Status
+void CPU::opcode_plp(uint16_t const& value) {
+  state.flags = stack_pop();
+}
+
+
+// ROL - Rotate Left (Memory)
+void CPU::opcode_rol(uint16_t const& value) {
+  uint8_t m = memory[value] << 1;
+  if(is_carry_set(state.flags)) m |= 0x01;
+
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_carry(memory[value] & 0x80, state.flags);
+  set_negative(m & 0x80, state.flags);
+  memory[value] = m;
+}
+
+
+// ROL - Rotate Left (Accumulator)
+void CPU::opcode_rol_acc(uint16_t const& value) {
+  uint8_t a = state.a << 1;
+  if(is_carry_set(state.flags)) a |= 0x01;
+
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_carry(state.a & 0x80, state.flags);
+  set_negative(a & 0x80, state.flags);
+  state.a = a;
+}
+
+
+// ROR - Rotate Right (Memory)
+void CPU::opcode_ror(uint16_t const& value) {
+  uint8_t m = memory[value] >> 1;
+  if(is_carry_set(state.flags)) m |= 0x80;
+
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_carry(memory[value] & 0x01, state.flags);
+  set_negative(m & 0x80, state.flags);
+  memory[value] = m;
+}
+
+
+// ROR - Rotate Right (Accumulator)
+void CPU::opcode_ror_acc(uint16_t const& value) {
+  uint8_t a = state.a >> 1;
+  if(is_carry_set(state.flags)) a |= 0x80;
+
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_carry(state.a & 0x01, state.flags);
+  set_negative(a & 0x80, state.flags);
+  state.a = a;
+}
+
+
+// RTI - Return from Interrupt
+void CPU::opcode_rti(uint16_t const& value) {
+  state.flags = stack_pop();
+  uint8_t low = stack_pop();
+  state.pc = (stack_pop() << 8) | low;
+}
+
+
+// RTS - Return from Subroutine
+void CPU::opcode_rts(uint16_t const& value) {
+  uint8_t low = stack_pop();
+  state.pc = (stack_pop() << 8) | low;
+}
+
+
+// SBC - Subtract with Carry
+void CPU::opcode_sbc(uint16_t const& value) {
+  uint16_t sbc = state.a - memory[value] - !is_carry_set(state.flags);
+
+  // BCD Support
+  if(is_decimal_set(state.flags)) {
+    // Obtain the 9’s compliment of the bcd subtrahend
+    uint8_t comp = 0x63 - to_bcd(memory[value]);
+    // Add the bcd minuend to the 9's compliment of the subtrahand
+    sbc = to_bcd(state.a - !is_carry_set(state.flags)) + comp;
+    // Add the carry if present
+    if(comp & 0x100) sbc += 1;
+  }
+
+  // Check flags
+  set_zero(!(sbc & 0xFF), state.flags);
+  set_negative(sbc & 0x80, state.flags);  // If top bit set number is negative
+  set_overflow(!((state.a ^ memory[value]) & 0x80) && ((state.a ^ sbc) & 0x80), state.flags);
+  set_carry(sbc > 0xFF, state.flags);
+
+  state.a = sbc & 0xFF;
+}
+
+
+// SEC - Set Carry Flag
+void CPU::opcode_sec(uint16_t const& value) {
+  set_carry(true, state.flags);
+}
+
+
+// SED - Set Decimal Flag
+void CPU::opcode_sed(uint16_t const& value) {
+  set_decimal(true, state.flags);
+}
+
+
+// SEI - Set Interrupt Disable
+void CPU::opcode_sei(uint16_t const& value) {
+  set_interrupt(true, state.flags);
+}
+
+
+// STA - Store Accumulator
+void CPU::opcode_sta(uint16_t const& value) {
+  memory[value] = state.a;
+}
+
+
+// STX - Store X Register
+void CPU::opcode_stx(uint16_t const& value) {
+  memory[value] = state.x;
+}
+
+
+// STY - Store Y Register
+void CPU::opcode_sty(uint16_t const& value) {
+  memory[value] = state.y;
+}
+
+
+// TAX - Transfer Accumulator to X
+void CPU::opcode_tax(uint16_t const& value) {
+  state.x = state.a;
+  // Check flags
+  set_zero(!(state.x & 0xFF), state.flags);
+  set_negative(state.x & 0x80, state.flags);
+}
+
+
+// TAY - Transfer Accumulator to Y
+void CPU::opcode_tay(uint16_t const& value) {
+  state.y = state.a;
+  // Check flags
+  set_zero(!(state.y & 0xFF), state.flags);
+  set_negative(state.y & 0x80, state.flags);
+}
+
+
+// TSX - Transfer Stack Pointer to X
+void CPU::opcode_tsx(uint16_t const& value) {
+  state.x = state.sp;
+  // Check flags
+  set_zero(!(state.x & 0xFF), state.flags);
+  set_negative(state.x & 0x80, state.flags);
+}
+
+
+// TXA - Transfer X to Accumulator
+void CPU::opcode_txa(uint16_t const& value) {
+  state.a = state.x;
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_negative(state.a & 0x80, state.flags);
+}
+
+
+// TXS - Transfer X to Stack Pointer
+void CPU::opcode_txs(uint16_t const& value) {
+  state.sp = state.x;
+}
+
+
+// TYA - Transfer Y to Accumulator
+void CPU::opcode_tya(uint16_t const& value) {
+  state.a = state.y;
+  // Check flags
+  set_zero(!(state.a & 0xFF), state.flags);
+  set_negative(state.a & 0x80, state.flags);
+}
